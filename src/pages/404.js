b1e1f8b0f5e1f4ba29f699ea36e13aff5ee1b307
Tong Yin <tongyin002@gmail.com>
2020-05-15 17:35:42 -0700

    yeah

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,24 +1,18 @@
 import React from "react";
 
-import Layout from "../components/layout";
 import SEO from "../components/seo";
-import abductionIllustration from "../images/abduction-illustration.svg";
+import LayoutBlack from "../components/layoutBlack";
 
 function NotFoundPage() {
   return (
-    <Layout>
+    <LayoutBlack>
       <SEO title="404: Not found" />
       <div>
-        <img
-          alt="Ghost getting abducted by aliens"
-          className="block mx-auto w-1/2"
-          src={abductionIllustration}
-        />
-        <h2 className="bg-yellow-400 text-2xl font-bold inline-block my-8 p-3">
+        <h2 className="bg-yellow-400 text-5xl text-white font-bold inline-block my-8 p-3">
           Looks like this page is a ghost that got abducted by aliens...
         </h2>
       </div>
-    </Layout>
+    </LayoutBlack>
   );
 }
 
